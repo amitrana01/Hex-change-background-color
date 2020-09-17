aa14b97bfdc08474354e2c58abbec063a4df83fc
Amit Rana <amitrana01@gmail.com>
2020-09-18 00:39:57 +0530

    app.js complete

---
+++ app.js
@@ -0,0 +1,23 @@
+const clickMe = document.querySelector('.btn');
+const hexValue = document.getElementById('hex-code');
+
+let colorArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+clickMe.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('clicked');
+  let c1 = colorArr[colorIndex()];
+  let c2 = colorArr[colorIndex()];
+  let c3 = colorArr[colorIndex()];
+  let c4 = colorArr[colorIndex()];
+  let c5 = colorArr[colorIndex()];
+  let c6 = colorArr[colorIndex()];
+  let hexCode = `#${c1}${c2}${c3}${c4}${c5}${c6}`;
+
+  document.body.style.backgroundColor = hexCode;
+  hexValue.innerText = hexCode;
+});
+
+function colorIndex() {
+  return Math.floor(Math.random() * Math.floor(colorArr.length - 1));
+}
